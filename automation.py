f3cb23c6ab15d2996a022b8562327220d1545246
alamgir09 <alamgir@usc.edu>
2023-11-22 22:28:31 -0500

    Finished script, working currently

---
+++ automation.py
@@ -0,0 +1,22 @@
+import os 
+import shutil
+import glob
+
+desktop_path = os.path.expanduser('~/Desktop')
+screenshots_folder = os.path.expanduser('~/Desktop/Screenshots')
+
+
+# Create Screenshots folder if doesn't exist
+if not os.path.exists(screenshots_folder):
+    os.makedirs(screenshots_folder)
+
+# Pattern for screen shots
+pattern = "Screen Shot *"
+
+# Move the screenshots
+for screenshot in glob.glob(os.path.join(desktop_path, pattern)):
+    shutil.move(screenshot, screenshots_folder)
+    print(f"Moved: {screenshot}")
+
+print("All screenshots have been moved.")
+
